19ea11ce4acb95e61e739a89093a9fedbcc699b2
AM\mfriese1 <michael_friesen@trimble.com>
2022-03-07 12:24:07 -0800

    End of Section 10 (Persistence)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,6 +11,8 @@ using System.Web;
 using System;
 using API.DTOs;
 using AutoMapper;
+using System.Security.Claims;
+using System.Collections.Specialized;
 
 namespace API.Controllers
 {
@@ -39,6 +41,21 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
